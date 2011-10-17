f6b81e8e378f4787915f78a91ad400090f188443
Ted Reed <ted.reed@gmail.com>
2011-10-16 23:42:07 -0700

    Add a gtkrc that colors gvim's background the same as the actual background if the cols/rows don't line up just right

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-for file in xsession xmobarrc bashrc bash_aliases vimrc vim gitconfig; do
+for file in xsession xmobarrc bashrc bash_aliases vimrc vim gitconfig gtkrc-2.0; do
 	ln -sf "$(pwd)/$file" ~/.$file
 done
