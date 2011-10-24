af36e43e035eeb11bf0cf989ebd66e3c9351926e
Ted Reed <ted.reed@gmail.com>
2011-10-24 10:30:51 -0700

    Guard linking vim directory so that it doesn't print an error

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ for file in xsession xmobarrc bashrc bash_aliases vimrc gitconfig gtkrc-2.0; do
 	ln -sf "$(pwd)/$file" ~/.$file
 done
 
-ln -sfT "$(pwd)/vim/" ~/.vim/
+test -d ~/.vim/ || ln -sfT "$(pwd)/vim/" ~/.vim/
