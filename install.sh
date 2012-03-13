ac7978dcb54626bad7c08d6a4bbbcd56b9813717
Ted Reed <ted.reed@gmail.com>
2012-03-13 00:39:10 -0700

    Add global git ignore (cvsignore) per tpope's suggestion

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-for file in Xmodmap xsession xmobarrc bashrc bash_aliases vimrc gitconfig gtkrc-2.0; do
+for file in cvsignore Xmodmap xsession xmobarrc bashrc bash_aliases vimrc gitconfig gtkrc-2.0; do
 	ln -sf "$(pwd)/$file" ~/.$file
 done
 
