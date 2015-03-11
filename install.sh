ee9ff8e5ea0098fb2062c80cf753277fcb3bf8b2
Ted Reed <ted.reed@gmail.com>
2015-03-11 01:58:24 -0700

    fix fzf install on linux

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 for file in tmux.conf cvsignore Xmodmap xsession xmobarrc bashrc shellrc vimrc vimrc-plugins gitconfig gtkrc-2.0 zshrc; do
 	ln -sf "$(pwd)/$file" ~/.$file
 done
@@ -9,5 +11,10 @@ done
 if uname -a | grep -q Darwin; then
     brew install fzf
 else
-    curl -o ~/bin/fzf https://github.com/junegunn/fzf-bin/releases/download/0.9.4/fzf-0.9.4-linux_amd64.tgz
+    wget -O /tmp/fzf.tgz https://github.com/junegunn/fzf-bin/releases/download/0.9.4/fzf-0.9.4-linux_amd64.tgz
+    pushd ~/bin > /dev/null
+    tar xf /tmp/fzf.tgz
+    mv fzf* fzf
+    popd > /dev/null
+    rm /tmp/fzf.tgz
 fi
