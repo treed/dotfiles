6cce020936a972d2961ba16ae0733be39f6f0b59
Ted Reed <ted.reed@gmail.com>
2016-03-21 17:55:57 -0700

    spacemacs config updates

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -36,15 +36,18 @@ values."
      javascript
      git
      go
+     haskell
      markdown
      org
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom)
+     shell-scripts
      spacemacs-layouts
      semantic
      spell-checking
      syntax-checking
+     themes-megapack
      typescript
      version-control
      )
@@ -90,7 +93,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark spacemacs-light)
+   dotspacemacs-themes '(spacemacs-light wombat)
    ;; If non nil the cursor color matches the state color.
    dotspacemacs-colorize-cursor-according-to-state nil
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
