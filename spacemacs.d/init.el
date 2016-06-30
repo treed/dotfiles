3dd88bf7a6bf18f7309a63ce202528c32586b179
Ted Reed <ted.reed@gmail.com>
2016-06-30 01:39:14 -0700

    add yaml

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -50,6 +50,7 @@ values."
      themes-megapack
      typescript
      version-control
+     yaml
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
