705e185f72e69a695bf6f6c70509fded26ed1150
Ted Reed <treed@imvu.com>
2016-10-25 12:55:13 -0700

    remove docker-machine support; not needed anymore

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,3 @@ set -U fish_greeting
 eval (thefuck --alias | tr '\n' ';')
 set --universal fish_user_paths $fish_user_paths ~/bin
 alias edit="emacsclient -t"
-set docker_machine (which docker-machine ^/dev/null)
-if test -n "$docker_machine"
-    eval (docker-machine env default | sed 's/-x/-xU/')
-end
