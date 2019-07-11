7d0d62d6dd8c6a758a06ec3dc81fade61dc573cd
Ted Reed <treed@purestorage.com>
2019-07-11 11:49:27 -0700

    handle backups better

--- init.el
+++ init.el
@@ -369,6 +369,27 @@
 ;; Get rid of the toolbar
 (tool-bar-mode -1)
 
+;; Tidy up backups
+(setq
+  ; Don't litter the filesystem with backup files
+  backup-directory-alist `((".*" . ,temporary-file-directory))
+  auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+  ; Use versioned backups
+  delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
+
+(message "Deleting old backup files...")
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
