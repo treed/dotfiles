ac8af6a6e552ab2b06437724acbfaeb4424d0aa4
Ted Reed <treed@purestorage.com>
2019-07-11 16:27:13 -0700

    Something about this doesn't actually work (fifth is void?)

--- init.el
+++ init.el
@@ -380,16 +380,6 @@
   kept-old-versions 2
   version-control t)
 
-(message "Deleting old backup files...")
-(let ((week (* 60 60 24 7))
-      (current (float-time (current-time))))
-  (dolist (file (directory-files temporary-file-directory t))
-    (when (and (backup-file-name-p file)
-               (> (- current (float-time (fifth (file-attributes file))))
-                  week))
-      (message "%s" file)
-      (delete-file file))))
-
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
